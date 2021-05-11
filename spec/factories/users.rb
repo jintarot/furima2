6cbb55edb71e8c7f2b403dd246485f5d8da73fae
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-11 13:44:45 +0900

    ；

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    email                 {Faker::Internet.email}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     first_name            {'田中'}
     first_name_kana       {'太郎'}
     last_name             {'タナカ'}
     last_name_kana        {'タロウ'}
-    birthdata             {'1980-5-7'}
+    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     before do
       @user = FactoryBot.build(:user)
+      @email = @user.email
     end
   describe "ユーザー新規登録" do
     context '登録できない時' do
     it 'nicknameが空では登録できない' do
-     boolen = @user.valid?
-    
+      user = User.new(password: "123456",nickname:"", birthdata: "1966-05-06", last_name: "山田", first_name: "太郎", last_name_kana: "ヤマダ", first_name_kana: "タロウ", email: "", password:"123456")
+      user.valid?
+     
+    expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
       user = User.new(password: "123456", birthdata: "1966-05-06", last_name: "山田", first_name: "太郎", last_name_kana: "ヤマダ", first_name_kana: "タロウ", email: "", password:"123456")
@@ -17,7 +20,22 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
       # emailが空では登録できないテストコードを記述します
-    
+    it 'emailに＠がなければいけない' do
+      @user.email = 'helloworldgamil.com'
+  
+  @user.valid?
+  
+  expect(@user.errors.full_messages).to include('Email is invalid')
+    end
+    it 'emailは一意でないといけない'do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+  end
+
+
     it 'last_nameが空では登録できない' do
       user = User.new(password: "123456", birthdata: "1966-05-06", last_name: "", first_name: "太郎", last_name_kana: "ヤマダ", first_name_kana: "タロウ", email: "wtswnhnjn123@icloud.com",password:"123456")
       user.valid?
@@ -49,6 +67,26 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Last name kana is invalid. Input full-width characters.")
     end
+    it '半角数字のみでは登録できない' do
+    user = User.new( birthdata: "1966-05-06", last_name: "山田", first_name: "太郎", last_name_kana: "kaka", first_name_kana: "かか", email: "wtswnhnjn123@icloud.com",password:"９９９９９９９９９９９")
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name kana is invalid. Input full-width characters.")
+  end
+  it '半角英語のみでは登録できない' do
+    user = User.new( birthdata: "1966-05-06", last_name: "山田", first_name: "太郎", last_name_kana: "kaka", first_name_kana: "かか", email: "wtswnhnjn123@icloud.com",password:"９９９９９９９９９９９")
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name kana is invalid. Input full-width characters.")
+  end
+  it '生年月日がなければ登録できない' do
+    user = User.new( birthdata: "1966-05-06", last_name: "山田", first_name: "太郎", last_name_kana: "kaka", first_name_kana: "かか", email: "wtswnhnjn123@icloud.com",password:"９９９９９９９９９９９")
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name kana is invalid. Input full-width characters.")
+  end
+  
+    
+    
+    
+    
   end
     context '登録できる時' do
     it 'nickname,password,password_confirmation,last_name,last_name_kana,first_name,first_name_kanaがあれば登録できる' do
