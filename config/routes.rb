037981feebc67c0312e644b3e5c387ce5df0e699
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-12 23:33:32 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "items#index"
   devise_for :users
+  root "items#index"
   resources :items,only:[:new,:create,:edit,:update,:show]
     #resources :orders, only:[:index,:create]
   #end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_132814) do
+ActiveRecord::Schema.define(version: 2021_05_12_143007) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
