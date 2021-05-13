64423d412b73270c115205e71d73ca495a24956e
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-13 13:54:25 +0900

    validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
   
+  with_options precense: true do
+     validates :name
+      validates :item_text
+      validates :price
+    with_options numericality:{other_than:0} do
+      validates :category_id
+      validates :status_id
+      validates :shipping_id
+      validates :day_id
+    end
+  end
 end
