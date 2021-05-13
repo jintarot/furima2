a75018974df194525723b17d899e1f1ad82cef45
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-13 18:51:30 +0900

    user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    factory :user do
+      nickname              {Faker::Name.initials(number: 2)}
+      email                 {Faker::Internet.email}
+      password              {'1a' + Faker::Internet.password(min_length: 6)}
+      password_confirmation {password}
+      first_name            {'田中'}
+      first_name_kana       {'太郎'}
+      last_name             {'タナカ'}
+      last_name_kana        {'タロウ'}
+      birthdate            {"2021-5-11"}
+      end
   end
 end
