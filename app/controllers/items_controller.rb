e1822a8ee4071f431aa0a711005a5aef0f129024
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-16 19:21:18 +0900

    q

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! ,only:[:new,:create]
+  before_action :authenticate_user! ,only:[:new,:create,:edit,:update]
+  
   def new
     @item = Item.new
   end
@@ -11,7 +12,17 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+  def edit
+    @item = Item.new
+  end
+  def update
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
   private
   def item_params
     params.require(:item).permit(:image,:name,:item_text,:prefecture_id,:category_id,:status_id,:day_id,:shipping_id,:prefecture_id,:price).merge(user_id:current_user.id)
