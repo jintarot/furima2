d4d7388373aa3054a2e4fde799e7e8d3b5d10bfe
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-16 19:53:29 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! ,only:[:new,:create,:edit,:update]
-  
+  def index
+    @items = Item.all
+  end
   def new
     @item = Item.new
   end
