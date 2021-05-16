5b60ad8538e86340e639b5f245e361f3b72cab9e
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-16 19:12:05 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! ,only:[:new,:create]
+  before_action :authenticate_user! ,only:[:new,:create,:edit,:update]
   def index
     @items = Item.all.order(created_at:"DESC")
   end
@@ -15,12 +15,25 @@ class ItemsController < ApplicationController
     end
   end
   def show
-    @item = Item.find(params[:id])
+   find_item
   end
-  
+  def edit
+    @item = Item.new
+  end
+  def update
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+  end
   private
   def item_params
     params.require(:item).permit(:image,:name,:item_text,:prefecture_id,:category_id,:status_id,:day_id,:shipping_id,:prefecture_id,:price).merge(user_id:current_user.id)
   end
-  
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
