fa828d4f8b0501afd220a066a2f91e3821ff6efb
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-18 02:23:58 +0900

    a

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @io = ItemOrder.new(io_params)
     if @io.valid?
-   
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price
+        card: io_params[:token]
+        currency: "jpy"
+      )
      @io.save
       redirect_to root_path
     end
@@ -15,6 +20,6 @@ class OrdersController < ApplicationController
   private
   def io_params
     @item = Item.find(params[:item_id])
-    params.permit(:postal_code,:prefecture_id,:city,:adress,:building,:phone_number).merge(token:params[:token],user_id:current_user.id,item_id:@item.id)
+    params.permit(:postal_code,:prefecture_id,:city,:adress,:building,:phone_number,:token).merge(,user_id:current_user.id,item_id:@item.id)
   end
 end
