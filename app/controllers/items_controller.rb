a9f94910b5b494fd9f0f11a3f06b627dbd9b7e34
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-17 17:19:55 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! ,only:[:new,:create,:edit,:update]
-  before_action :find_item,only:[:show,:not_collect_user]
+  before_action :find_item,only:[:show,:edit,:update]
   before_action :not_collect_user, only:[:edit,:update]
   def index
     @items = Item.all.order(created_at:"DESC")
