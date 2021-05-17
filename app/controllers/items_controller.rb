79cbfa2468f0aac1c4e599486a2bf9e5f3a86165
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-17 15:47:10 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! ,only:[:new,:create,:edit,:update]
+  before_action :find_item, only:[:show,:not_collect_user]
   befere_action :not_collect_user, only:[:edit,:update]
+  
   def index
 
     @items = Item.all.order(created_at:"DESC")
@@ -17,7 +19,7 @@ class ItemsController < ApplicationController
     end
   end
   def show
-   find_item
+  
   end
   def edit
 
@@ -40,7 +42,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
   def not_collect_user
-    @item = Item.find(params[:id])
+    
     if @item.user != current_user
       redirect_to root_path
     end
