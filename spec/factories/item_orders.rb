0000034b43c9ce4efe20d947fa7470a81803d749
jintarot <71145959+jintarot@users.noreply.github.com>
2021-05-21 15:11:41 +0900

    修正

--- spec/factories/item_orders.rb
+++ spec/factories/item_orders.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item_order do
-    postal_code       {"1710051"}
+    postal_code       {"171-0051"}
     prefecture_id     {1}
     city              {"神戸"}
     adress            {"1111"}
